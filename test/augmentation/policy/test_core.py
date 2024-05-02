0184a0847ba22121e04c21cf58429a3d34bc93e1
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:52:01 +0600

    improve

--- test/augmentation/policy/test_core.py
+++ test/augmentation/policy/test_core.py
@@ -2,18 +2,24 @@ import unittest
 
 from snorkel.augmentation import ApplyAllPolicy, ApplyEachPolicy
 
-
 class TestPolicy(unittest.TestCase):
     def test_apply_each_policy(self):
-        policy = ApplyEachPolicy(3, keep_original=True)
-        samples = policy.generate_for_example()
-        self.assertEqual(samples, [[], [0], [1], [2]])
-
-        policy = ApplyEachPolicy(3, keep_original=False)
-        samples = policy.generate_for_example()
-        self.assertEqual(samples, [[0], [1], [2]])
+        test_cases = [
+            (3, True, [[], [0], [1], [2]]),
+            (3, False, [[0], [1], [2]]),
+        ]
+        for num\_choices, keep\_original, expected\_samples in test\_cases:
+            with self.subTest(num\_choices=num\_choices, keep\_original=keep\_original):
+                policy = ApplyEachPolicy(num\_choices, keep\_original=keep\_original)
+                samples = policy.generate\_for\_example()
+                self.assertEqual(samples, expected\_samples)
 
     def test_apply_all_policy(self):
-        policy = ApplyAllPolicy(3, n_per_original=2, keep_original=False)
-        samples = policy.generate_for_example()
-        self.assertEqual(samples, [[0, 1, 2], [0, 1, 2]])
+        test\_cases = [
+            (3, 2, False, [[0, 1, 2], [0, 1, 2]]),
+        ]
+        for num\_choices, n\_per\_original, keep\_original, expected\_samples in test\_cases:
+            with self.subTest(num\_choices=num\_choices, n\_per\_original=n\_per\_original, keep\_original=keep\_original):
+                policy = ApplyAllPolicy(num\_choices, n\_per\_original=n\_per\_original, keep\_original=keep\_original)
+                samples = policy.generate\_for\_example()
+                self.assertEqual(samples, expected\_samples)
