c220dfe9448f07c6031af1aaedf3f97c737a6733
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:50:34 +0600

    comment

--- snorkel/labeling/model/__init__.py
+++ snorkel/labeling/model/__init__.py
@@ -1,2 +1,8 @@
+# Importing various classes related to voting and label models from the baselines
+# and label_model modules. These classes are used for implementing different
+# voting and label model strategies in a machine learning context.
+# The 'noqa: F401' comment is used to suppress the F401 warning from flake8,
+# which is triggered because the imported modules are not used directly in
+# this file.
 from .baselines import MajorityClassVoter, MajorityLabelVoter, RandomVoter  # noqa: F401
 from .label_model import LabelModel  # noqa: F401
