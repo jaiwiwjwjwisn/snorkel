dfe22c52bc87a0c90bd56b8c55c3ccbf5b01dbfb
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:50:32 +0600

    comment

--- snorkel/classification/training/loggers/__init__.py
+++ snorkel/classification/training/loggers/__init__.py
@@ -1,4 +1,2 @@
-from .checkpointer import Checkpointer, CheckpointerConfig  # noqa: F401
-from .log_manager import LogManager, LogManagerConfig  # noqa: F401
-from .log_writer import LogWriter, LogWriterConfig  # noqa: F401
-from .tensorboard_writer import TensorBoardWriter  # noqa: F401
+# Import the necessary classes and functions for checkpointing, logging, and tensorboard writing
+from .checkpointer import Checkpointer, CheckpointerConfig  # noqa: F40
