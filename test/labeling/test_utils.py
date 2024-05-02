d924881fd857e709ee0e454d548ed59b5cd8fe4b
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:50:53 +0600

    improve

--- test/labeling/test_utils.py
+++ test/labeling/test_utils.py
@@ -7,14 +7,28 @@ from snorkel.labeling import filter_unlabeled_dataframe
 
 
 class TestAnalysis(unittest.TestCase):
-    def test_filter_unlabeled_dataframe(self) -> None:
-        X = pd.DataFrame(dict(A=["x", "y", "z"], B=[1, 2, 3]))
-        y = np.array(
+    """Tests for the `filter_unlabeled_dataframe` function."""
+
+    def setUp(self) -> None:
+        """Create test data."""
+        self.X = pd.DataFrame(dict(A=["x", "y", "z"], B=[1, 2, 3]))
+        self.y = np.array(
             [[0.25, 0.25, 0.25, 0.25], [1.0, 0.0, 0.0, 0.0], [0.2, 0.3, 0.5, 0.0]]
         )
-        L = np.array([[0, 1, -1], [-1, -1, -1], [1, 1, 0]])
-        X_filtered, y_filtered = filter_unlabeled_dataframe(X, y, L)
-        np.array_equal(X_filtered.values, np.array([["x", 1], ["z", 3]]))
+        self.L = np.array([[0, 1, -1], [-1, -1, -1], [1, 1, 0]])
+
+    def test_filter_unlabeled_dataframe_some_positive_labels(self) -> None:
+        """Test filtering unlabeled dataframe with some positive labels."""
+        X_filtered, y_filtered = filter_unlabeled_dataframe(self.X, self.y, self.L)
+        np.testing.assert_array_equal(X_filtered.values, np.array([["x", 1], ["z", 3]]))
         np.testing.assert_array_almost_equal(
             y_filtered, np.array([[0.25, 0.25, 0.25, 0.25], [0.2, 0.3, 0.5, 0.0]])
         )
+
+    def test_filter_unlabeled_dataframe_no_positive_labels(self) -> None:
+        """Test filtering unlabeled dataframe with no positive labels."""
+        self.L[:, 0] = -1
+        X_filtered, y_filtered = filter_unlabeled_dataframe(self.X, self.y, self.L)
+        np.testing.assert_array_equal(X_filtered, pd.DataFrame(columns=self.X.columns))
+        np.testing.assert_array_equal(y_filtered, np.zeros((0, self.y.shape[1])))
+
