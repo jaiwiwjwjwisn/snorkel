faedd00ac28f14fe074d0ea0f0a74a87cac73c6b
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:50:39 +0600

    comment

--- snorkel/slicing/apply/core.py
+++ snorkel/slicing/apply/core.py
@@ -1,19 +1,32 @@
+# Import the LFApplier and PandasLFApplier classes from the snorkel.labeling module.
 from snorkel.labeling import LFApplier, PandasLFApplier
 
 
+# Define a new SFApplier class that inherits from LFApplier.
 class SFApplier(LFApplier):
     """SF applier for a list of data points.
 
-    See ``snorkel.labeling.core.LFApplier`` for details.
+    This class is a specialized version of the LFApplier class, designed to
+    apply Snorkel's labeling functions to a list of data points. The _use_recarray
+    attribute is set to True to use NumPy recarray for better performance.
+
+    For more information about the LFApplier class, refer to
+    ``snorkel.labeling.core.LFApplier``.
     """
 
     _use_recarray = True
 
 
+# Define a new PandasSFApplier class that inherits from PandasLFApplier.
 class PandasSFApplier(PandasLFApplier):
     """SF applier for a Pandas DataFrame.
 
-    See ``snorkel.labeling.core.PandasLFApplier`` for details.
+    This class is a specialized version of the PandasLFApplier class, designed to
+    apply Snorkel's labeling functions to a Pandas DataFrame. The _use_recarray
+    attribute is set to True to use NumPy recarray for better performance.
+
+    For more information about the PandasLFApplier class, refer to
+    ``snorkel.labeling.core.PandasLFApplier``.
     """
 
     _use_recarray = True
