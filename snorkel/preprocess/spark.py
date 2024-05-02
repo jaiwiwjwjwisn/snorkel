f513319991307de90f8c9966de6aedd11901d9b3
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:50:44 +0600

    improve

--- snorkel/preprocess/spark.py
+++ snorkel/preprocess/spark.py
@@ -1,1 +1,12 @@
-from snorkel.map.spark import make_spark_mapper as make_spark_preprocessor  # noqa: F401
+from snorkel.map.spark import make_spark_mapper  # noqa: F401
+
+def make_spark_preprocessor():
+    """
+    A function to create a Spark mapper for use in Snorkel.
+    
+    This function uses the `make_spark_mapper` function from the `snorkel.map.spark` module to create a Spark mapper.
+    
+    Returns:
+        A Spark mapper object.
+    """
+    return make_spark_mapper()
