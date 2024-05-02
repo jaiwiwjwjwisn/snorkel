bd4c2a0b6c6b6263482bcf178bf4ed3d99d0f25b
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:50:54 +0600

    improve

--- test/labeling/apply/lf_applier_spark_test_script.py
+++ test/labeling/apply/lf_applier_spark_test_script.py
@@ -1,44 +1,19 @@
 """
-This script is used to manually test
-    `snorkel.labeling.apply.lf_applier_spark.SparkLFApplier`
-
-To test on AWS EMR:
-    1. Allocate an EMR cluster (e.g. label 5.24.0) with > 1 worker and SSH permissions
-    2. Clone and pip install snorkel on the master node
-        ```
-        sudo yum install git
-        git clone https://github.com/snorkel-team/snorkel
-        cd snorkel
-        python3 -m pip install -t snorkel-package .
-        cd snorkel-package
-        zip -r ../snorkel-package.zip .
-        cd ..
-        ```
-    3. Run
-        ```
-        sudo sed -i -e \
-            '$a\\export PYSPARK_PYTHON=/usr/bin/python3' \
-            /etc/spark/conf/spark-env.sh
-        ```
-    4. Run
-        ```
-        spark-submit \
-            --py-files snorkel-package.zip \
-            test/labeling/apply/lf_applier_spark_test_script.py
-        ```
+This script is used to manually test `snorkel.labeling.apply.lf_applier_spark.SparkLFApplier`.
 """
 
 import logging
-from typing import List
-
+import sys
 import numpy as np
 from pyspark import SparkContext
+from pyspark.errors import SparkException
 
 from snorkel.labeling.apply.spark import SparkLFApplier
 from snorkel.labeling.lf import labeling_function
 from snorkel.types import DataPoint
 
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 @labeling_function()
@@ -54,14 +29,24 @@ def g(x: DataPoint, db: List[int]) -> int:
 DATA = [3, 43, 12, 9]
 L_EXPECTED = np.array([[0, 1], [1, 0], [0, 0], [0, 1]])
 
+def get_spark_context() -> SparkContext:
+    try:
+        sc = SparkContext()
+        sc.addPyFile("snorkel-package.zip")
+        return sc
+    except SparkException as e:
+        logger.error(f"Error creating SparkContext: {e}")
+        sys.exit(1)
+
 
 def build_lf_matrix() -> None:
-    logging.info("Getting Spark context")
-    sc = SparkContext()
-    sc.addPyFile("snorkel-package.zip")
+    logger.info("Getting Spark context")
+    sc = get_spark_context()
+
+    logger.info("Creating RDD")
     rdd = sc.parallelize(DATA)
 
-    logging.info("Applying LFs")
+    logger.info("Applying LFs")
     lf_applier = SparkLFApplier([f, g])
     L = lf_applier.apply(rdd)
 
