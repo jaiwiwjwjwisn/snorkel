e3d73423885599cdc79880e059ad2f1a260c6cf4
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:51:34 +0600

    improve

--- snorkel/slicing/sliceaware_classifier.py
+++ snorkel/slicing/sliceaware_classifier.py
@@ -4,42 +4,48 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
+from typing import Any, Dict, List, Optional, Set, Union
 
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
 from snorkel.analysis import Scorer
 from snorkel.classification import DictDataLoader, DictDataset, Operation, Task
 from snorkel.classification.data import DEFAULT_INPUT_DATA_KEY, DEFAULT_TASK_NAME
 from snorkel.classification.multitask_classifier import MultitaskClassifier
-
 from .utils import add_slice_labels, convert_to_slice_tasks
 
 
 class SliceAwareClassifier(MultitaskClassifier):
-    """A slice-aware classifier that supports training + scoring on slice labels.
+    """A slice-aware classifier that supports training and scoring on slice labels.
 
     NOTE: This model currently only supports binary classification.
 
     Parameters
     ----------
-    base_architecture
+    base_architecture : nn.Module
         A network architecture that accepts input data and outputs a representation
-    head_dim
+    head_dim : int
         Output feature dimension of the base_architecture, and input dimension of the
         internal prediction head: ``nn.Linear(head_dim, 2)``.
-    slice_names
-        A list of slice names that the model will accept initialize as tasks
-        and accept as corresponding labels
-    scorer
-        A Scorer to be used for initialization of the ``MultitaskClassifier`` superclass.
-    **multitask_kwargs
+    slice_names : List[str]
+        A list of slice names that the model will accept initialize as input and
+        accept as corresponding labels
+    scorer_kwargs : dict
+        Keyword arguments to be passed to the Scorer constructor.
+
+        See ``snorkel.analysis.Scorer`` for more details.
+    multitask_kwargs : dict
         Arbitrary keyword arguments to be passed to the ``MultitaskClassifier`` superclass.
 
     Attributes
     ----------
-    base_task
+    base_task : Task
         A base ``snorkel.classification.Task`` that the model will learn.
         This becomes a ``master_head_module`` that combines slice tasks information.
         For more, see ``snorkel.slicing.convert_to_slice_tasks``.
-    slice_names
+    slice_names : List[str]
         See above
     """
 
@@ -50,9 +56,12 @@ class SliceAwareClassifier(MultitaskClassifier):
         slice_names: List[str],
         input_data_key: str = DEFAULT_INPUT_DATA_KEY,
         task_name: str = DEFAULT_TASK_NAME,
-        scorer: Scorer = Scorer(metrics=["accuracy", "f1"]),
+        scorer_kwargs: dict = None,
         **multitask_kwargs: Any,
     ) -> None:
+        scorer_kwargs = scorer_kwargs or {}
+        scorer = Scorer(metrics=["accuracy", "f1"], **scorer_kwargs)
+
         # Initialize module_pool with 1) base_architecture and 2) prediction_head
         # Assuming `head_dim` can be used to map base_architecture to prediction_head
         module_pool = nn.ModuleDict(
@@ -85,9 +94,15 @@ class SliceAwareClassifier(MultitaskClassifier):
         # Convert base_task to associated slice_tasks
         slice_tasks = convert_to_slice_tasks(self.base_task, slice_names)
 
-        # Initialize a MultitaskClassifier with all slice_tasks
+        # Initialize a MultitaskClassifier with all slice_tasks set to use the
+        # same prediction head as the base task
         model_name = f"{task_name}_sliceaware_classifier"
-        super().__init__(tasks=slice_tasks, name=model_name, **multitask_kwargs)
+        super().__init__(
+            tasks=slice_tasks,
+            name=model_name,
+            prediction_head=slice_tasks[0].prediction_head,
+            **multitask_kwargs,
+        )
         self.slice_names = slice_names
 
     def make_slice_dataloader(
@@ -97,15 +112,12 @@ class SliceAwareClassifier(MultitaskClassifier):
 
         Parameters
         ----------
-        dataset
+        dataset : DictDataset
             A DictDataset that will be converted into a slice-aware dataloader
-        S
+        S : np.recarray
             A [num_examples, num_slices] slice matrix indicating whether
             each example is in every slice
-        slice_names
-            A list of slice names corresponding to columns of ``S``
-
-        dataloader_kwargs
+        dataloader_kwargs : dict
             Arbitrary kwargs to be passed to DictDataLoader
             See ``DictDataLoader.__init__``.
         """
@@ -126,7 +138,10 @@ class SliceAwareClassifier(MultitaskClassifier):
 
     @torch.no_grad()
     def score_slices(
-        self, dataloaders: List[DictDataLoader], as_dataframe: bool = False
+        self,
+        dataloaders: List[DictDataLoader],
+        as_dataframe: bool = False,
+        eval_slices_on_base_task: bool = True,
     ) -> Union[Dict[str, float], pd.DataFrame]:
         """Scores appropriate slice labels using the overall prediction head.
 
@@ -139,34 +154,31 @@ class SliceAwareClassifier(MultitaskClassifier):
 
         Parameters
         ----------
-        dataloaders
+        dataloaders : List[DictDataLoader]
             A list of DictDataLoaders to calculate scores for
-        as_dataframe
+        as_dataframe : bool
             A boolean indicating whether to return results as pandas
             DataFrame (True) or dict (False)
-        eval_slices_on_base_task
+        eval_slices_on_base_task : bool
             A boolean indicating whether to remap slice labels to base task.
             Otherwise, keeps evaluation of slice labels on slice-specific heads.
 
         Returns
         -------
-        Dict[str, float]
-            A dictionary mapping metric¡ names to corresponding scores
+        Union[Dict[str, float], pd.DataFrame]
+            A dictionary mapping metric names to corresponding scores
             Metric names will be of the form "task/dataset/split/metric"
         """
 
         eval_mapping: Dict[str, Optional[str]] = {}
         # Collect all labels
-        all_labels: Union[List, Set] = []
-        for dl in dataloaders:
-            all_labels.extend(dl.dataset.Y_dict.keys())  # type: ignore
-        all_labels = set(all_labels)
+        all_labels = {label for dl in dataloaders for label in dl.dataset.Y_dict}  # type: ignore
 
         # By convention, evaluate on "pred" labels, not "ind" labels
         # See ``snorkel.slicing.utils.add_slice_labels`` for more about label creation
         for label in all_labels:
             if "pred" in label:
-                eval_mapping[label] = self.base_task.name
+                eval_mapping[label] = self.base_task.name if eval_slices_on_base_task else label
             elif "ind" in label:
                 eval_mapping[label] = None
 
