f14b1550b92d0768651e34f6bba32735e64ef8be
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:51:04 +0600

    improve

--- scripts/check_requirements.py
+++ scripts/check_requirements.py
@@ -111,73 +111,55 @@ def main() -> int:
     requirements_all = dict(requirements_essential, **requirements_other)
     setup_essential, setup_test, essential_duplicates, test_duplicates = parse_setup()
 
-    if requirements_duplicate:
-        exit_code = 1
-        for module in requirements_duplicate:
-            print(f"  ✗ '{module}' appears more than once in requirements.txt")
+    print_issues(
+        "Requirements.txt",
+        requirements_duplicate,
+        essential_duplicates,
+        requirements_all,
+        setup_essential,
+    )
 
-    if essential_duplicates:
-        exit_code = 1
-        for module in essential_duplicates:
-            print(
-                f"  ✗ '{module}' appears more than once in 'install_requires' "
-                f"section of setup.py"
-            )
-
-    if test_duplicates:
-        exit_code = 1
-        for module in test_duplicates:
-            print(
-                f"  ✗ '{module}' appears more than once in 'tests_require' "
-                f"section of setup.py"
-            )
-
-    # Find all packages listed as essential in requirements.txt that differ
-    # in or are absent from setup.py.
-    for module, version in requirements_essential.items():
-        if module not in setup_essential:
-            exit_code = 1
-            print(
-                f"  ✗ '{module}' listed as essential in requirements.txt "
-                f"but is missing from 'install_requires' field of setup.py"
-            )
-        elif setup_essential[module] != version:
-            exit_code = 1
-            print(
-                f"  ✗ '{module}' has version '{version}' in requirements.txt but "
-                f"'{setup_essential[module]}' in 'install_requires' field of setup.py"
-            )
-
-    # Find all packages listed under 'install_requires' in setup.py that are not
-    # listed as essential in requirements.txt.
-    for module, version in setup_essential.items():
-        if module in requirements_other:
-            exit_code = 1
-            print(
-                f"  ✗ '{module}' appears under 'install_requires' in setup.py but "
-                f"is listed as non-essential in requirements.txt"
-            )
-        elif module not in requirements_essential:
-            exit_code = 1
-            print(f"  ✗ '{module}' appears in setup.py but not in requirements.txt")
-
-    # Find all packages listed under 'tests_require' in setup.py that are missing
-    # from requirements.txt, or have a version discrepancy.
-    for module, version in setup_test.items():
-        if module not in requirements_all:
-            exit_code = 1
-            print(
-                f"  ✗ '{module}' appears under 'tests_require' in setup.py "
-                f"but is missing from requirements.txt"
-            )
-        elif requirements_all[module] != version:
-            exit_code = 1
-            print(
-                f"  ✗ '{module}' has version '{version}' in 'tests_require' field of "
-                f"setup.py but '{requirements_all[module]}' in requirements.txt"
-            )
+    print_issues(
+        "Setup.py",
+        essential_duplicates,
+        test_duplicates,
+        setup_essential,
+        requirements_all,
+    )
 
     return exit_code
+
+
+def print_issues(
+    header: str,
+    duplicates: Set[str],
+    section_duplicates: Set[str],
+    packages1: PackagesType,
+    packages2: PackagesType,
+) -> None:
+    print(f"\n### {header} issues:")
+
+    if duplicates:
+        print_duplicates("Packages", duplicates)
+
+    if section_duplicates:
+        print_duplicates("Sections", section_duplicates)
+
+    for module, version in packages1.items():
+        if module not in packages2:
+            print(f"  ✗ '{module}' is missing in {header}")
+        elif packages2[module] != version:
+            print(
+                f"  ✗ '{module}' has version '{version}' in {header} but "
+                f"'{packages2[module]}' in requirements.txt"
+            )
+
+
+def print_duplicates(title: str, duplicates: Set[str]) -> None:
+    if duplicates:
+        print(f"  ✗ Duplicate {title}:")
+        for module in duplicates:
+            print(f"      - {module}")
 
 
 if __name__ == "__main__":
