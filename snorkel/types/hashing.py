22c7274ac0461b04b54755b59127b69e58094b87
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:50:39 +0600

    comment

--- snorkel/types/hashing.py
+++ snorkel/types/hashing.py
@@ -1,4 +1,9 @@
 from collections.abc import Hashable
 from typing import Any, Callable
 
+# Define a type alias for a hashing function
 HashingFunction = Callable[[Any], Hashable]
+
+# A hashing function is a callable object that takes in any object as an argument
+# and returns a hashable object. This is often used in data structures like
+# hash tables and hash sets to quickly and efficiently map keys to values.
