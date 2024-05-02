ea459a40bddb2129c662fc22b08c3f1c6e50a36d
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:51:06 +0600

    improve

--- snorkel/types/classifier.py
+++ snorkel/types/classifier.py
@@ -1,3 +1,8 @@
 from typing import NamedTuple
 
-Config = NamedTuple
+class Config(NamedTuple):
+    """A named tuple for configuration settings."""
+    # Configuration settings go here as class variables
+    # for example:
+    # some_setting = 0
+    # another_setting = 1.5
