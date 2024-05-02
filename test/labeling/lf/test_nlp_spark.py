f93401a90b121c5ebc008a2d243c8d1bcfa0d3cc
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:50:53 +0600

    improve

--- test/labeling/lf/test_nlp_spark.py
+++ test/labeling/lf/test_nlp_spark.py
@@ -11,37 +11,40 @@ from snorkel.labeling.lf.nlp_spark import (
 )
 from snorkel.types import DataPoint
 
-
 def has_person_mention(x: DataPoint) -> int:
-    person_ents = [ent for ent in x.doc.ents if ent.label_ == "PERSON"]
-    return 0 if len(person_ents) > 0 else -1
-
+    return 0 if any(ent.label_ == "PERSON" for ent in x.doc.ents) else -1
 
 @pytest.mark.spark
 class TestNLPLabelingFunction(unittest.TestCase):
-    def _run_lf(self, lf: SparkNLPLabelingFunction) -> None:
-        x = Row(num=8, text="The movie is really great!")
-        self.assertEqual(lf(x), -1)
-        x = Row(num=8, text="Jane Doe acted well.")
-        self.assertEqual(lf(x), 0)
+    def _run_lf(self, lf: SparkNLPLabelingFunction, text: str) -> None:
+        x = Row(num=8, text=text)
+        self.assertEqual(lf(x), has_person_mention(SimpleNamespace(num=8, text=text)))
 
     def test_nlp_labeling_function(self) -> None:
         lf = SparkNLPLabelingFunction(name="my_lf", f=has_person_mention)
-        self._run_lf(lf)
+        self._run_lf(lf, "The movie is really great!")
+        self._run_lf(lf, "Jane Doe acted well.")
 
-    def test_nlp_labeling_function_decorator(self) -> None:
+    @pytest.mark.parametrize(
+        "text",
+        [
+            "The movie is really great!",
+            "Jane Doe acted well.",
+        ],
+    )
+    def test_nlp_labeling_function_decorator(self, text: str) -> None:
         @spark_nlp_labeling_function()
-        def has_person_mention(x: DataPoint) -> int:
-            person_ents = [ent for ent in x.doc.ents if ent.label_ == "PERSON"]
-            return 0 if len(person_ents) > 0 else -1
+        def has_person_mention_decorator(x: DataPoint) -> int:
+            return has_person_mention(x)
 
-        self.assertIsInstance(has_person_mention, SparkNLPLabelingFunction)
-        self.assertEqual(has_person_mention.name, "has_person_mention")
-        self._run_lf(has_person_mention)
+        self.assertIsInstance(has_person_mention_decorator, SparkNLPLabelingFunction)
+        self.assertEqual(has_person_mention_decorator.name, "has_person_mention")
+        self._run_lf(has_person_mention_decorator, text)
 
     def test_spark_nlp_labeling_function_with_nlp_labeling_function(self) -> None:
-        # Do they have separate _nlp_configs?
         lf = NLPLabelingFunction(name="my_lf", f=has_person_mention)
         lf_spark = SparkNLPLabelingFunction(name="my_lf_spark", f=has_person_mention)
         self.assertEqual(lf(SimpleNamespace(num=8, text="Jane Doe acted well.")), 0)
-        self._run_lf(lf_spark)
+
+        self._run_lf(lf_spark, "The movie is really great!")
+        self._run_lf(lf_spark, "Jane Doe acted well.")
