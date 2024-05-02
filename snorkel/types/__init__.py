19bb0cfa2fed9c9e958e66278b367530c0b8722b
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:50:40 +0600

    comment

--- snorkel/types/__init__.py
+++ snorkel/types/__init__.py
@@ -1,3 +1,8 @@
 from .classifier import Config  # noqa: F401
+# Importing Config from the classifier module. This class contains configuration settings for the classifier.
+
 from .data import DataPoint, DataPoints, Field, FieldMap  # noqa: F401
+# Importing several classes from the data module. DataPoint represents a single data point, while DataPoints is a collection of DataPoint objects. Field and FieldMap are used to define and map data fields.
+
 from .hashing import HashingFunction  # noqa: F401
+# Importing HashingFunction from the hashing module. This class is used to define various hashing functions for data processing.
