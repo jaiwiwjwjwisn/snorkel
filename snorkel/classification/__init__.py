171c1488b81a00f12737ef7e80e7197bc5bce9f4
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:50:50 +0600

    improve

--- snorkel/classification/__init__.py
+++ snorkel/classification/__init__.py
@@ -1,10 +1,14 @@
 """PyTorch-based multi-task learning framework for discriminative modeling."""
 
-from .data import DictDataLoader, DictDataset  # noqa: F401
-from .loss import cross_entropy_with_probs  # noqa: F401
-from .multitask_classifier import MultitaskClassifier  # noqa: F401
-from .task import Operation, Task  # noqa: F401
-from .training.loggers import (  # noqa: F401
+from .data import (
+    DictDataLoader,
+    DictDataset,
+)
+from .loss import cross_entropy_with_probs
+from .multitask_classifier import MultitaskClassifier
+from .task import Operation, Task
+
+from .training.loggers import (
     Checkpointer,
     CheckpointerConfig,
     LogManager,
@@ -13,4 +17,4 @@ from .training.loggers import (  # noqa: F401
     LogWriterConfig,
     TensorBoardWriter,
 )
-from .training.trainer import Trainer  # noqa: F401
+from .training.trainer import Trainer
