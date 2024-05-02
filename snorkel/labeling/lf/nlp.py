4c0e745682743595b6d394fce9ef3181d72377a7
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:51:02 +0600

    improve

--- snorkel/labeling/lf/nlp.py
+++ snorkel/labeling/lf/nlp.py
@@ -1,4 +1,6 @@
-from typing import Any, Callable, List, Mapping, NamedTuple, Optional
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any, Callable, ClassVar, Dict, List, Mapping, Optional
 
 from snorkel.preprocess import BasePreprocessor
 from snorkel.preprocess.nlp import EN_CORE_WEB_SM, SpacyPreprocessor
@@ -6,29 +8,38 @@ from snorkel.types import HashingFunction
 
 from .core import LabelingFunction, labeling_function
 
+class Language(Enum):
+    EN = "en_core_web_sm"
 
-class SpacyPreprocessorParameters(NamedTuple):
-    """Parameters needed to construct a SpacyPreprocessor.
-
-    See ``snorkel.preprocess.nlp.SpacyPreprocessor``.
-    """
+class SpacyPreprocessorParameters:
+    """Parameters needed to construct a SpacyPreprocessor."""
 
     text_field: str
     doc_field: str
-    language: str
-    disable: Optional[List[str]]
-    pre: List[BasePreprocessor]
-    memoize: bool
-    memoize_key: Optional[HashingFunction]
-    gpu: bool
+    language: Language
+    disable: Optional[List[str]] = None
+    pre: List[BasePreprocessor] = field(default_factory=list)
+    memoize: bool = True
+    memoize_key: Optional[HashingFunction] = None
+    gpu: bool = False
 
-
-class SpacyPreprocessorConfig(NamedTuple):
+class SpacyPreprocessorConfig:
     """Tuple of SpacyPreprocessor and the parameters used to construct it."""
 
-    nlp: SpacyPreprocessor
+    nlp: ClassVar[SpacyPreprocessor]
     parameters: SpacyPreprocessorParameters
 
+    @classmethod
+    def create(cls, parameters: SpacyPreprocessorParameters) -> "SpacyPreprocessorConfig":
+        """Create and store a SpacyPreprocessor instance."""
+        nlp = SpacyPreprocessor(**parameters.__dict__)
+        cls.nlp = nlp
+        return cls(nlp=nlp, parameters=parameters)
+
+    @property
+    def preprocessors(self) -> List[BasePreprocessor]:
+        """Return the list of preprocessors."""
+        return [self.nlp] + self.parameters.pre
 
 class BaseNLPLabelingFunction(LabelingFunction):
     """Base class for spaCy-based LFs."""
@@ -36,38 +47,37 @@ class BaseNLPLabelingFunction(LabelingFunction):
     _nlp_config: SpacyPreprocessorConfig
 
     @classmethod
-    def _create_preprocessor(
+    def create_preprocessor(
         cls, parameters: SpacyPreprocessorParameters
     ) -> SpacyPreprocessor:
+        """Create a SpacyPreprocessor instance."""
         raise NotImplementedError
 
     @classmethod
-    def _create_or_check_preprocessor(
+    def create_or_check_preprocessor(
         cls,
         text_field: str,
         doc_field: str,
-        language: str,
-        disable: Optional[List[str]],
-        pre: List[BasePreprocessor],
-        memoize: bool,
-        memoize_key: Optional[HashingFunction],
-        gpu: bool,
+        language: Language,
+        disable: Optional[List[str]] = None,
+        pre: List[BasePreprocessor] = None,
+        memoize: bool = True,
+        memoize_key: Optional[HashingFunction] = None,
+        gpu: bool = False,
     ) -> None:
-        # Create a SpacyPreprocessor if one has not yet been instantiated.
-        # Otherwise, check that configuration matches already instantiated one.
+        """Create or check a SpacyPreprocessor instance."""
         parameters = SpacyPreprocessorParameters(
             text_field=text_field,
             doc_field=doc_field,
             language=language,
             disable=disable,
-            pre=pre,
+            pre=pre or [],
             memoize=memoize,
             memoize_key=memoize_key,
             gpu=gpu,
         )
         if not hasattr(cls, "_nlp_config"):
-            nlp = cls._create_preprocessor(parameters)
-            cls._nlp_config = SpacyPreprocessorConfig(nlp=nlp, parameters=parameters)
+            cls._nlp_config = SpacyPreprocessorConfig.create(parameters)
         elif parameters != cls._nlp_config.parameters:
             raise ValueError(
                 f"{cls.__name__} already configured with different parameters: "
@@ -82,24 +92,23 @@ class BaseNLPLabelingFunction(LabelingFunction):
         pre: Optional[List[BasePreprocessor]] = None,
         text_field: str = "text",
         doc_field: str = "doc",
-        language: str = EN_CORE_WEB_SM,
+        language: Language = Language.EN,
         disable: Optional[List[str]] = None,
         memoize: bool = True,
         memoize_key: Optional[HashingFunction] = None,
         gpu: bool = False,
     ) -> None:
-        self._create_or_check_preprocessor(
+        self.create_or_check_preprocessor(
             text_field,
             doc_field,
             language,
             disable,
-            pre or [],
+            pre,
             memoize,
             memoize_key,
             gpu,
         )
-        super().__init__(name, f, resources=resources, pre=[self._nlp_config.nlp])
-
+        super().__init__(name, f, resources=resources, pre=self._nlp_config.preprocessors)
 
 class NLPLabelingFunction(BaseNLPLabelingFunction):
     r"""Special labeling function type for spaCy-based LFs.
@@ -175,109 +184,7 @@ class NLPLabelingFunction(BaseNLPLabelingFunction):
     """
 
     @classmethod
-    def _create_preprocessor(
+    def create_preprocessor(
         cls, parameters: SpacyPreprocessorParameters
     ) -> SpacyPreprocessor:
-        return SpacyPreprocessor(**parameters._asdict())
-
-
-class base_nlp_labeling_function(labeling_function):
-    """Decorator to define a BaseNLPLabelingFunction child object from a function."""
-
-    _lf_cls: Optional[type] = None
-
-    def __init__(
-        self,
-        name: Optional[str] = None,
-        resources: Optional[Mapping[str, Any]] = None,
-        pre: Optional[List[BasePreprocessor]] = None,
-        text_field: str = "text",
-        doc_field: str = "doc",
-        language: str = EN_CORE_WEB_SM,
-        disable: Optional[List[str]] = None,
-        memoize: bool = True,
-        memoize_key: Optional[HashingFunction] = None,
-        gpu: bool = False,
-    ) -> None:
-        super().__init__(name, resources, pre)
-        self.text_field = text_field
-        self.doc_field = doc_field
-        self.language = language
-        self.disable = disable
-        self.memoize = memoize
-        self.memoize_key = memoize_key
-        self.gpu = gpu
-
-    def __call__(self, f: Callable[..., int]) -> BaseNLPLabelingFunction:
-        """Wrap a function to create an ``BaseNLPLabelingFunction``.
-
-        Parameters
-        ----------
-        f
-            Function that implements the core NLP LF logic
-
-        Returns
-        -------
-        BaseNLPLabelingFunction
-            New ``BaseNLPLabelingFunction`` executing logic in wrapped function
-        """
-        if self._lf_cls is None:
-            raise NotImplementedError("_lf_cls must be defined")
-        name = self.name or f.__name__
-        return self._lf_cls(
-            name=name,
-            f=f,
-            resources=self.resources,
-            pre=self.pre,
-            text_field=self.text_field,
-            doc_field=self.doc_field,
-            language=self.language,
-            disable=self.disable,
-            memoize=self.memoize,
-            memoize_key=self.memoize_key,
-            gpu=self.gpu,
-        )
-
-
-class nlp_labeling_function(base_nlp_labeling_function):
-    """Decorator to define an NLPLabelingFunction object from a function.
-
-    Parameters
-    ----------
-    name
-        Name of the LF
-    resources
-        Labeling resources passed in to ``f`` via ``kwargs``
-    pre
-        Preprocessors to run before SpacyPreprocessor is executed
-    text_field
-        Name of data point text field to input
-    doc_field
-        Name of data point field to output parsed document to
-    language
-        spaCy model to load
-        See https://spacy.io/usage/models#usage
-    disable
-        List of pipeline components to disable
-        See https://spacy.io/usage/processing-pipelines#disabling
-    memoize
-        Memoize preprocessor outputs?
-    memoize_key
-        Hashing function to handle the memoization (default to snorkel.map.core.get_hashable)
-
-    Example
-    -------
-    >>> @nlp_labeling_function()
-    ... def has_person_mention(x):
-    ...     person_ents = [ent for ent in x.doc.ents if ent.label_ == "PERSON"]
-    ...     return 0 if len(person_ents) > 0 else -1
-    >>> has_person_mention
-    NLPLabelingFunction has_person_mention, Preprocessors: [SpacyPreprocessor...]
-
-    >>> from types import SimpleNamespace
-    >>> x = SimpleNamespace(text="The movie was good.")
-    >>> has_person_mention(x)
-    -1
-    """
-
-    _lf_cls = NLPLabelingFunction
+        return SpacyPreprocessor(**parameters.__dict
