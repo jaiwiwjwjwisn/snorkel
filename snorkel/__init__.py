775fe65324a41155db1f58c2d2a01d4b44ed4c4e
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:51:06 +0600

    improve

--- snorkel/__init__.py
+++ snorkel/__init__.py
@@ -1,1 +1,9 @@
-from .version import VERSION as __version__  # noqa: F401
+# Import the version module and assign the version number to a variable
+from .version import VERSION
+
+# Assign the variable to the __version__ variable in this module using the
+# built-in __version__ variable as a reference
+__version__ = VERSION
+
+# Suppress the F401 import warning for the __version__ variable
+#
