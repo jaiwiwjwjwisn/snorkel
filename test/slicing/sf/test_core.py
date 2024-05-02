868632618fbf514eedcb23bc4250e18ffecfa931
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:51:35 +0600

    improve

--- test/slicing/sf/test_core.py
+++ test/slicing/sf/test_core.py
@@ -1,30 +1,26 @@
 import unittest
 from types import SimpleNamespace
-
 from snorkel.slicing import SlicingFunction, slicing_function
 
-
 class TestSlicingFunction(unittest.TestCase):
-    def _run_sf(self, sf: SlicingFunction) -> None:
-        x_43 = SimpleNamespace(num=43)
-        x_19 = SimpleNamespace(num=19)
-        self.assertEqual(sf(x_43), True)
-        self.assertEqual(sf(x_19), False)
+    def _run_sf(self, sf: SlicingFunction, x: SimpleNamespace) -> None:
+        self.assertEqual(sf(x), x.num > 42)
 
-    def _run_sf_raise(self, sf: SlicingFunction) -> None:
-        x_none = SimpleNamespace(num=None)
+    def _run_sf_raise(self, sf: SlicingFunction, x: SimpleNamespace) -> None:
         with self.assertRaises(TypeError):
-            sf(x_none)
+            sf(x)
 
     def test_slicing_function_decorator(self) -> None:
         @slicing_function()
-        def sf(x) -> int:
+        def sf(x) -> bool:
             return x.num > 42
 
         self.assertIsInstance(sf, SlicingFunction)
         self.assertEqual(sf.name, "sf")
-        self._run_sf(sf)
-        self._run_sf_raise(sf)
+        x_43 = SimpleNamespace(num=43)
+        x_19 = SimpleNamespace(num=19)
+        self._run_sf(sf, x_43)
+        self._run_sf_raise(sf, SimpleNamespace(num=None))
 
     def test_slicing_function_decorator_no_parens(self) -> None:
         with self.assertRaisesRegex(ValueError, "missing parentheses"):
