1417964e9ed1b1331ef900f2f6a5d6dfd6e5f521
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:51:47 +0600

    improve

--- test/map/test_spark.py
+++ test/map/test_spark.py
@@ -3,19 +3,9 @@ from typing import Optional
 
 import pytest
 from pyspark.sql import Row
+from pyspark.sql.functions import lower
 
-from snorkel.map import Mapper, lambda_mapper
+from snorkel.map import Mapper, field_map, lambda_mapper
 from snorkel.map.spark import make_spark_mapper
 from snorkel.types import DataPoint, FieldMap
-
-# A class that inherits from Mapper and defines a constructor with custom
-# arguments. The `run` method is expected to return a FieldMap with the
-# transformed fields.
-class SplitWordsMapper(Mapper):
-    def __init__(
-        self, name: str, text_field: str, lower_field: str, words_field: str
-    ) -> None:
-        super().__init__(
-            name, dict(text=text_field), dict(lower=lower_field, words=words_field)
-        )
 
