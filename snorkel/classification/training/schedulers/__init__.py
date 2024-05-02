05dbe0803535ccc7052e1e087667a306867b0515
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:50:35 +0600

    comment

--- snorkel/classification/training/schedulers/__init__.py
+++ snorkel/classification/training/schedulers/__init__.py
@@ -1,4 +1,8 @@
+# Import the SequentialScheduler and ShuffledScheduler classes from their respective modules.
 from .sequential_scheduler import SequentialScheduler
 from .shuffled_scheduler import ShuffledScheduler
 
+# Define a dictionary called batch_schedulers that maps string keys to class values.
+# The keys are "sequential" and "shuffled", and the corresponding values are the
+# SequentialScheduler and ShuffledScheduler classes, respectively.
 batch_schedulers = {"sequential": SequentialScheduler, "shuffled": ShuffledScheduler}
