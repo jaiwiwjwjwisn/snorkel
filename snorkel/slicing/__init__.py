98eebb7271790dc675c7b7839e8fc88bdeeeaf94
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:50:47 +0600

    improve

--- snorkel/slicing/__init__.py
+++ snorkel/slicing/__init__.py
@@ -1,8 +1,21 @@
-"""Programmatic data set slicing: SF creation, monitoring utilities, and representation learning for slices."""
+"""
+Module: slicing
+----------------
 
-from .apply.core import PandasSFApplier, SFApplier  # noqa: F401
-from .modules.slice_combiner import SliceCombinerModule  # noqa: F401
-from .monitor import slice_dataframe  # noqa: F401
-from .sf.core import SlicingFunction, slicing_function  # noqa: F401
-from .sliceaware_classifier import SliceAwareClassifier  # noqa: F401
-from .utils import add_slice_labels, convert_to_slice_tasks  # noqa: F401
+This module contains classes and functions for programmatic data set slicing,
+including:
+
+- SlicingFunctions for creating slices
+- PandasSFApplier and SFApplier for applying slicing functions to pandas dataframes
+- SliceCombinerModule for combining slices
+- SliceAwareClassifier for training classifiers on slices
+- slice_dataframe for monitoring slice data
+- add_slice_labels and convert_to_slice_tasks for data preparation
+"""
+
+from .apply.core import PandasSFApplier, SFApplier
+from .modules.slice_combiner import SliceCombinerModule
+from .monitor import slice_dataframe
+from .sf.core import SlicingFunction, slicing_function
+from .sliceaware_classifier import SliceAwareClassifier
+from .utils import add_slice_labels, convert_to_slice_tasks
